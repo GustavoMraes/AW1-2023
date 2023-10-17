9208be6b433d020282fed9e7faa8482a6054d86d
Gustavo Henrique de Moraes <104281604+GustavoMraes@users.noreply.github.com>
2023-10-17 15:15:36 -0300

    Praticando Express
    Lembrar de importar as pastas com o comando:
    
    npm install express cors mysql nodemon
    
    pq a pasta fica muito grande e não sobre no github

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import rotasUsuario from "./rotas/rotasUsuario.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/User", rotasUsuario)
+
+app.listen(8081)
